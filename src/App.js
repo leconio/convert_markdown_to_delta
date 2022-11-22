b978f25453b25dbbcaab59e64ac3bc8e6e0d8611
Spawn <leconio@outlook.com>
2022-11-22 09:18:34 +0000

    init project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import markdownToDelta from "markdown-to-quill-delta";
+
 
 function App() {
+
+  const [result, setResult] = useState('');
+
+  useEffect(() => {
+
+  })
+
+  const textInput = useRef(null);
+
+  function convert() {
+    const ops = markdownToDelta(textInput.current.value);
+    console.info("ops: ", JSON.stringify(ops))
+    setResult(JSON.stringify(ops))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <div className="App" style={{ display: "flex" }}>
+      <div style={{ width: "50%", minHeight: 200, }}>
+        <label for="editor">Convert your markdown to Delta</label>
+        <textarea name="editor" ref={textInput} style={{ width: "100%", minHeight: 200, }} />
+        <button onClick={convert}>提交</button>
+      </div>
+      <div style={{ width: 10 }}></div>
+      <div style={{ width: "50%", minHeight: 200, }} >
+        {result}
+      </div>
+    </div >
   );
 }
 
